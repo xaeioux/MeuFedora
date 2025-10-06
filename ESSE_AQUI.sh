87f7a46a3511ad49c576d671d2d1c5df66635355
Mateus Vinicius <46939526+xaeioux@users.noreply.github.com>
2025-10-06 07:19:01 -0300

    Update ESSE_AQUI.sh

--- ESSE_AQUI.sh
+++ ESSE_AQUI.sh
@@ -357,15 +357,6 @@ function _InstalaFlatpak() {
   flatpak install -y com.discordapp.Discord
 }
 
-function _InstalaVSCodium() {
-  sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-  printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
-  sudo dnf install -y codium
-  codium --install-extension dcasella.monokai-plusplus
-  codium --install-extension emmanuelbeziat.vscode-great-icons
-  codium --install-extension foxundermoon.shell-format
-}
-
 function _ConfiguraGrub() {
   HWVENDOR=$(hostnamectl | grep Vendor | awk {'print $3'})
   sudo mkdir /boot/grub2/themes
@@ -742,14 +733,6 @@ function _ConfereSistema() {
   echo -e $VERMELHOG '    ( >\¥/<)'$SEMCOR
   _Barra
   _ConfiguraGrub
-
-  echo "";
-  _Barra
-  echo -e $VERMELHOG '    (\_/)'
-  echo -e $VERMELHOG '   =( ಠﭛಠ)=  Instalando o VSCODIUM'
-  echo -e $VERMELHOG '    (>\¥/<)'$SEMCOR
-  _Barra
-  _InstalaVSCodium
 
   echo "";
   _Barra
